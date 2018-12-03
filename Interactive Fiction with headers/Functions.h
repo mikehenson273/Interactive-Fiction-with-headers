782237a8ac001bfccff4c05ff251bba3f7151292
MICHAEL HENSON <MICHENSO@uat.edu>
2018-12-02 17:05:08 -0700

    Add project files.

---
+++ Interactive Fiction with headers/Functions.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <Windows.h>
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cctype>
+
+using namespace std;
+
+//for allowing functions to be used in source
+void bedSearch();
+void GameEngine();
+void goAgain();
+void look();
+void mainEngine();
+void openDesk();
+void openDoor();
+void wait();
---
+++ Interactive Fiction with headers/Source.cpp
@@ -0,0 +1,16 @@
+#include "functions.h"
+
+using namespace std;
+
+
+
+int main()
+{
+	//makes full screen when activated
+	//SetConsoleDisplayMode(GetStdHandle(STD_OUTPUT_HANDLE), CONSOLE_FULLSCREEN_MODE, 0);
+	system("title Waking Up");
+	system("color 02"); //changes all console colors
+
+	mainEngine();
+	return 0;
+}
